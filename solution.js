1926ca996dfa1fe44ddfd9d205dd418f87a9db17
Shazammon <ammonorgill@gmail.com>
2022-12-13 10:22:39 -0700

    setting up embedded while loop

--- solution.js
+++ solution.js
@@ -11,12 +11,24 @@ const stringData = data.split(/\r?\n/)
 function findMatches() {
     // define all variables
     let strLength = 0
+    let match = false
+    let runsack = ''
     for (let i = 0; i < stringData.length; i++) {
         // find length of string and divide by two
+        runsack = stringData[i]
+        console.log(runsack)
+        strLength = runsack.length / 2
 
+        // console.log(strLength) 
         // for each character, compare to character of second string
+        // for (let j = 0; j < strLength; j++) {
+        //     while (match === false) {
+        //         console.log(stringData[j])
+        //     }
+        // }
         // if match is found, add to an array
         // find value of all letters in array
         // add all values together
     }
 }
+findMatches()
