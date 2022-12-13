5abb9a4cab7e451da369096332ee54346bb1f29a
Shazammon <ammonorgill@gmail.com>
2022-12-13 09:59:29 -0700

    wrote pseudo code for function

--- solution.js
+++ solution.js
@@ -6,4 +6,17 @@ let data = fs.readFileSync('data.txt', 'utf-8')
 
 const stringData = data.split(/\r?\n/)
 
-console.log(stringData)
+// console.log(stringData)
+
+function findMatches() {
+    // define all variables
+    let strLength = 0
+    for (let i = 0; i < stringData.length; i++) {
+        // find length of string and divide by two
+
+        // for each character, compare to character of second string
+        // if match is found, add to an array
+        // find value of all letters in array
+        // add all values together
+    }
+}
