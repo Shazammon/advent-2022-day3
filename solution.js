56f9b5b83ca909c8e39426a9e838a08ea3405cd3
Shazammon <ammonorgill@gmail.com>
2022-12-26 10:20:44 -0700

    updated function logic

--- solution.js
+++ solution.js
@@ -10,6 +10,8 @@ const stringData = data.split(/\r?\n/)
 
 //double finalized code 
 
+// write a funtion that cycles through each letter
+
 
 const letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
